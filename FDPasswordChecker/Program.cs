db507e0647feb065f46ec3c4f9a50f5e7c88899b
shan15dev <shansai@bluewin.ch>
2023-02-14 22:06:21 +0100

    function checks if password has uppercase letters

--- FDPasswordChecker/Program.cs
+++ FDPasswordChecker/Program.cs
@@ -1,3 +1,16 @@
-﻿public class Program
+﻿using FluentAssertions;
+using Xunit;
+
+public class Program
 {
+    [Theory]
+    [InlineData("asdfAsdf")]
+    void should_be_safe(string input)
+    {
+        var checkCaps = CheckCaps(input);
+
+        checkCaps.Should().Be(true);
+    }
+
+    private Func<string, bool> CheckCaps => x => x.Any(char.IsUpper);
 }
