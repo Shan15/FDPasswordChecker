9aa8119a06fbe5fd373c30259d00eb113f82f477
shan15dev <shansai@bluewin.ch>
2023-02-15 06:33:22 +0100

    And Fuc created -> Not working

--- FDPasswordChecker/Program.cs
+++ FDPasswordChecker/Program.cs
@@ -12,14 +12,15 @@ public class Program
         var checkSpecialCase = CheckSpecialCase(input);
         var checkNumber = CheckNumber(input);
 
-        checkUpper.Should().Be(true);
-        checkNumber.Should().Be(true);
-        checkLower.Should().Be(true);
-        checkSpecialCase.Should().Be(true);
+        var checker = And<string>(checkUpper, checkLower, checkSpecialCase, checkNumber);
+        
+        checker.Should().Be(true);
     }
 
     private static Func<string, bool> CheckUpper => x => x.Any(char.IsUpper);
     private static Func<string, bool> CheckLower => x => x.Any(char.IsLower);
     private static Func<string, bool> CheckNumber => x => x.Any(char.IsDigit);
     private static Func<string, bool> CheckSpecialCase => x => x.Any(c => !char.IsLetterOrDigit(c));
+    static Func<T, bool> And<T>(params Func<T, bool>[] checkers) =>
+        x => checkers.All(checker => checker(x));
 }
