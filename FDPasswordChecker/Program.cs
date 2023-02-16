ecfa7ee775a1686c77d13ae5cd63a924614e8c9b
smuraleetharan <smuraleetharan@kpmg.com>
2023-02-16 08:32:36 +0100

    Deleting not needed methods and use InlineData

--- FDPasswordChecker/Program.cs
+++ FDPasswordChecker/Program.cs
@@ -3,33 +3,23 @@ using Xunit;
 
 public class Program
 {
-    static List<string> GetFilteredPassword(Func<string, bool> filter)
-    {
-        var list = new List<string> { "apple", "Banan123", "hlaksdfjLKJALKSDF-1234" };
-
-        return list.Where(filter).ToList();
-    }
-
     [Theory]
-    [InlineData(1)]
-    void should_be_safe(int count)
+    [InlineData("apple", false)]
+    [InlineData("Banan123", false)]
+    [InlineData("hlaksdfjLKJALKSDF-1234", true)]
+    void should_be_safe(string password, bool isSafe)
     {
-        var checkUpper = CheckUpper();
-        var checkLower = CheckLower();
-        var checkSpecialCase = CheckSpecialCase();
-        var checkNumber = CheckNumber();
+        var checker = And(CheckUpper, CheckLower, CheckNumber, CheckSpecialCase, CheckUpper2);
 
-        var checker = And(checkUpper, checkLower, checkSpecialCase, checkNumber);
-        var actual = GetFilteredPassword(checker);
-        
-        actual.Count.Should().Be(count);
+        checker(password).Should().Be(isSafe);
     }
 
-    private static Func<string, bool> CheckUpper() => x => x.Any(char.IsUpper);
-    private static Func<string, bool> CheckLower() => x => x.Any(char.IsLower);
-    private static Func<string, bool> CheckNumber() => x => x.Any(char.IsDigit);
-    private static Func<string, bool> CheckSpecialCase() => x => x.Any(c => !char.IsLetterOrDigit(c));
+    private static Func<string, bool> CheckUpper => x => x.Any(char.IsUpper);
+    private static Func<string, bool> CheckLower => x => x.Any(char.IsLower);
+    private static Func<string, bool> CheckNumber => x => x.Any(char.IsDigit);
+    private static Func<string, bool> CheckSpecialCase => x => x.Any(c => !char.IsLetterOrDigit(c));
 
     static Func<T, bool> And<T>(params Func<T, bool>[] checkers) =>
         x => checkers.All(checker => checker(x));
+
 }
