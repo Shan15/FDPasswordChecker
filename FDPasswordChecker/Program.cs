474ccd247fd07c5a2d390d87329b177b11e63d48
shan15dev <shansai@bluewin.ch>
2023-02-16 20:17:17 +0100

    checks if chars are 8 or more

--- FDPasswordChecker/Program.cs
+++ FDPasswordChecker/Program.cs
@@ -9,7 +9,7 @@ public class Program
     [InlineData("hlaksdfjLKJALKSDF-1234", true)]
     void should_be_safe(string password, bool isSafe)
     {
-        var checker = And(CheckUpper, CheckLower, CheckNumber, CheckSpecialCase);
+        var checker = And(CheckUpper, CheckLower, CheckNumber, CheckSpecialCase, HasEnoughChars);
 
         checker(password).Should().Be(isSafe);
     }
@@ -18,6 +18,7 @@ public class Program
     private static Func<string, bool> CheckLower => x => x.Any(char.IsLower);
     private static Func<string, bool> CheckNumber => x => x.Any(char.IsDigit);
     private static Func<string, bool> CheckSpecialCase => x => x.Any(c => !char.IsLetterOrDigit(c));
+    private static Func<string, bool> HasEnoughChars => x => x.Length >= 8;
 
     static Func<T, bool> And<T>(params Func<T, bool>[] checkers) =>
         x => checkers.All(checker => checker(x));
